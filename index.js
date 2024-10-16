37d0286a0f618338aca83151ecae3203862c9c56
Vru01 <vrushalisangale16@gmail.com>
2024-10-16 15:27:25 +0530

    For server and response in JSON format.

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { PythonShell } = require('python-shell');
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+
+        if (!prompt) {
+            return res.status(400).json({ message: "Give me a proper prompt" });
+        }
+
+        let pyshell = new PythonShell('./main.py');
+
+        pyshell.send(prompt);
+
+        pyshell.on('message', async function (message) {
+            try {
+               console.log(message);
+                const parsedAnswer = JSON.parse(JSON.parse(message));
+
+                // Send the parsed object as a JSON response
+                return res.status(200).json(parsedAnswer);
+
+            } catch (err) {
+                return res.status(500).json({ message: "Failed to parse the response from Python." });
+            }
+        });
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
+
+
+
+app.post('/GetResult', (req, res) => {
+    try {
+        const answer = req.body.answer;
+
+        if (!answer) {
+            return res.status(400).json({ message: "Give me a proper answer" });
+        }
+
+        let pyshell = new PythonShell('./main.py');
+
+        pyshell.send(answer);
+
+        pyshell.on('message', async function (message) {
+            try {
+               console.log(message);
+                const parsedAnswer = JSON.parse(JSON.parse(message));
+
+                // Send the parsed object as a JSON response
+                return res.status(200).json(parsedAnswer);
+
+            } catch (err) {
+                return res.status(500).json({ message: "Failed to parse the response from Python." });
+            }
+        });
+
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
+
+
+
+
+// mongodb+srv://9297:<db_password>@cluster0.9tlk4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
---
+++ main.py
@@ -0,0 +1,14 @@
+from meta_ai_api import MetaAI
+import json
+
+ai = MetaAI()
+question = input()
+
+# Get response
+response = ai.prompt(message=question)
+
+# Convert JSON response to a single-line string
+cleaned_response = json.dumps(response["message"], separators=(',', ':'))
+
+# Print the result
+print(cleaned_response)
