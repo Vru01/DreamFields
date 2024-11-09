09ffedd9c1bd5bf081b2cb17a33235342b1d9cad
Pranav Surve <pranavsurve.0786@gmail.com>
2024-11-09 17:19:42 +0530

    updated .sh

--- Server/start-dev.sh
+++ Server/start-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+pip install meta_ai_api
 source ./venv/bin/activate
 nodemon index.js
--- frontend/src/components/Quiz/QuizPage.jsx
+++ frontend/src/components/Quiz/QuizPage.jsx
@@ -30,7 +30,7 @@ const Quiz = () => {
     const startQuiz = async () => {
         setLoading(true);
         try {
-            await axios.post('http://localhost:5000/api/v1/quiz/startQuiz');
+            await axios.post('https://dreamfields-phsk.onrender.com/api/v1/quiz/startQuiz');
             fetchQuiz();
             setLoading(false);
         } catch (error) {
@@ -42,7 +42,7 @@ const Quiz = () => {
     // Fetch Quiz Questions Function
     const fetchQuiz = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/v1/quiz/getQuiz');
+            const response = await axios.get('https://dreamfields-phsk.onrender.com/api/v1/quiz/getQuiz');
             setQuizData(response.data.quiz.questions);
             setQuizStarted(true);
         } catch (error) {
@@ -66,8 +66,8 @@ const Quiz = () => {
         setLoading(true);
         <Loader />
         try {
-            await axios.post('http://localhost:5000/api/v1/quiz/submitAnswers', { answers: selectedAnswers });
-            const recommendationResponse = await axios.get('http://localhost:5000/api/v1/quiz/getLatestRecommendation');
+            await axios.post('https://dreamfields-phsk.onrender.com/api/v1/quiz/submitAnswers', { answers: selectedAnswers });
+            const recommendationResponse = await axios.get('https://dreamfields-phsk.onrender.com/api/v1/quiz/getLatestRecommendation');
             setRecommendations(recommendationResponse.data.recommendation.recommendedFields || []);
             toast.success('Answers submitted successfully and recommendations fetched!', {
                 position: "top-right",
